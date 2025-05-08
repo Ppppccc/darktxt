7db2c537cf784087dd95ef9addf94589c7e84d7f
Dark Prince <110161707+Ppppccc@users.noreply.github.com>
2025-05-08 18:37:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@
 from os import environ
 
 API_ID = int(environ.get("API_ID", "25431437"))
-API_HASH = environ.get("API_HASH", "3b6235f4375b77e9ce448ebc3111aa50)
+API_HASH = environ.get("API_HASH", "3b6235f4375b77e9ce448ebc3111aa50")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7304826521:AAHzxYKKVxhvdmnBStPL-29FYtXJvGGYGgs")
 
 #WEBHOOK = True  # Don't change this
